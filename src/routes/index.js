16418d65418d607a1b01fe5145caa3e08ff0dd1b
Espero pueda subirlo <a2020111314@estudiantes.upsa.edu.bo>
2022-02-27 23:24:11 -0600

    first commit

---
+++ src/routes/index.js
@@ -0,0 +1,73 @@
+const { Router } = require('express');
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+const uuidv4 = require('uuid/v4');
+
+const json_favs = fs.readFileSync('src/favs.json', 'utf-8');
+let favs = JSON.parse(json_favs);
+
+
+router.get('/', (req, res) => {
+  console.log(req.body.entrada);
+  res.render('index', 
+  { favs, 
+    entrada : req.body.entrada,
+    
+  });
+});
+
+router
+
+router.post('/',(req, res) => {
+  console.log(req.body.entrada);
+  res.render('index', 
+  { favs, 
+    entrada : req.body.entrada,
+    
+  });
+});
+
+
+router.post('/:hacerPost', (req, res) => {
+
+  const { text, id_str} = req.body;
+  let date = new Date();
+    let fecha = String(date.getDate()) + String(date.getMonth()) +  + date.getFullYear();
+  if (!text || !id_str) {
+    res.status(400).send("Entries must have a title and body");
+    
+    return;
+  }
+  
+  var newfav = {
+    id:parseInt(req.body.id_str),
+    id_str,
+    text,
+    created_at: date
+  };
+
+    console.log(parseInt(req.body.id));
+
+  // add a new book to the array
+  favs.push(newfav);
+  
+
+  // saving the array in a file
+  const json_favs = JSON.stringify(favs);
+  fs.writeFileSync('src/favs.json', json_favs, 'utf-8');
+
+  res.redirect('/');
+});
+
+router.get('/delete/:id', (req, res) => {
+  favs = favs.filter(fav => fav.id != req.params.id);
+
+  // saving data
+  const json_favs = JSON.stringify(favs);
+  fs.writeFileSync('src/favs.json', json_favs, 'utf-8');
+
+  res.redirect('/')
+});
+
+module.exports = router;
